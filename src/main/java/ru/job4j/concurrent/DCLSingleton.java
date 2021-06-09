7fcecf3b602229374f2c9583cf4d51c30a0da2f1
Nefedova <Katerina163>
2021-06-09 09:22:54 +0400

    2. Модель памяти Java [475130]

--- src/main/java/ru/job4j/concurrent/DCLSingleton.java
+++ src/main/java/ru/job4j/concurrent/DCLSingleton.java
@@ -8,7 +8,11 @@ public class DCLSingleton {
 
     public static DCLSingleton instOf() {
         if (inst == null) {
-             inst = new DCLSingleton();
+             synchronized (DCLSingleton.class) {
+                 if (inst == null) {
+                     inst = new DCLSingleton();
+                 }
+             }
         }
         return inst;
     }
