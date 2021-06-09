3b232cab0b143ddd6ae38299ec481184e040216b
Nefedova <Katerina163>
2021-06-09 15:19:38 +0400

    3. Junit тест для блокирующей очереди. [475130]

--- src/main/java/ru/job4j/SimpleBlockingQueue.java
+++ src/main/java/ru/job4j/SimpleBlockingQueue.java
@@ -30,4 +30,8 @@ public class SimpleBlockingQueue<T> {
             return queue.poll();
         }
     }
+
+    public synchronized boolean isEmpty() {
+        return queue.isEmpty();
+    }
 }
--- src/test/java/ru/job4j/SimpleBlockingQueueTest.java
+++ src/test/java/ru/job4j/SimpleBlockingQueueTest.java
@@ -2,6 +2,12 @@ package ru.job4j;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.stream.IntStream;
+
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
 
@@ -24,5 +30,48 @@ public class SimpleBlockingQueueTest {
         second.join();
         assertThat(i[0], is(1));
         assertThat(i[1], is(2));
+    }
+
+    @Test
+    public void whenFetchAllThenGetIt() throws InterruptedException {
+        final CopyOnWriteArrayList<Integer> buffer = new CopyOnWriteArrayList<>();
+        final SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>();
+        Thread producer = new Thread(
+                () -> IntStream.range(0, 5).forEach(queue::offer));
+        producer.start();
+        Thread consumer = new Thread(
+                () -> {
+                    while (!queue.isEmpty() || !Thread.currentThread().isInterrupted()) {
+                        buffer.add(queue.poll());
+                    }
+                }
+        );
+        consumer.start();
+        producer.join();
+        consumer.interrupt();
+        consumer.join();
+        assertThat(buffer, is(Arrays.asList(0, 1, 2, 3, 4)));
+    }
+
+    @Test
+    public void testTwo() throws InterruptedException {
+        final Queue<String> buffer = new LinkedList<>();
+        final SimpleBlockingQueue<String> queue = new SimpleBlockingQueue<>();
+        Thread producer = new Thread(() -> {
+            queue.offer("0");
+            queue.offer("1");
+            queue.offer("2");
+        });
+        producer.start();
+        Thread consumer = new Thread(() -> {
+            while (!queue.isEmpty() || !Thread.currentThread().isInterrupted()) {
+                buffer.add(queue.poll());
+            }
+        });
+        consumer.start();
+        producer.join();
+        consumer.interrupt();
+        consumer.join();
+        assertThat(buffer, is(Arrays.asList("0", "1", "2")));
     }
 }
