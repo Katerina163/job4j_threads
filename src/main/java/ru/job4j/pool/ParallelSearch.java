c18ea2f31d57638a894454dc9f4eb76b5001762c
Nefedova <Katerina163>
2021-06-11 13:12:12 +0400

    3. ForkJoinPool [475130]

---
+++ src/main/java/ru/job4j/pool/ParallelSearch.java
@@ -0,0 +1,50 @@
+package ru.job4j.pool;
+
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RecursiveTask;
+
+public class ParallelSearch extends RecursiveTask<Integer> {
+    private final int[] array;
+    private final int first;
+    private final int last;
+    private final int number;
+
+    private ParallelSearch(int[] array, int first, int last, int number) {
+        this.array = array;
+        this.first = first;
+        this.last = last;
+        this.number = number;
+    }
+
+    @Override
+    protected Integer compute() {
+        if (last - first < 10) {
+            for (int i = 0; i < array.length; i++) {
+                if (array[i] == number) {
+                    return i;
+                }
+            }
+        }
+        int mid = (first + last) / 2;
+        ParallelSearch pleft = new ParallelSearch(array, first, mid, number);
+        ParallelSearch pright = new ParallelSearch(array, mid + 1, last, number);
+        pleft.fork();
+        pright.fork();
+        int left = pleft.join();
+        int right = pright.join();
+        if (left == -1) {
+            return right;
+        }
+        return left;
+    }
+
+    public static int search(int[] array, int number) {
+        ForkJoinPool fJP = new ForkJoinPool();
+        return fJP.invoke(new ParallelSearch(array, 0, array.length - 1, number));
+    }
+
+    public static void main(String[] args) {
+        int[] array = {1, 24, 2, 16, 3, 48, 9, 7, 8, 45, 18};
+        System.out.println(ParallelSearch.search(array, 18));
+    }
+}
